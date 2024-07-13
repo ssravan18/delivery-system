4a7c21d8a33f2837abbd835b63c686fdb4e1f513
ssravan18 <111529264+ssravan18@users.noreply.github.com>
2024-07-13 10:36:04 +0530

    Update app-routing.module.ts

--- delivery-system/delivery-system-frontend/src/app/app-routing.module.ts
+++ delivery-system/delivery-system-frontend/src/app/app-routing.module.ts
@@ -9,11 +9,13 @@ import { OrderPlacementComponent } from './customer/order-placement/order-placem
 import { OrderTrackingComponent } from './customer/order-tracking/order-tracking.component';
 import { FeedbackComponent } from './customer/feedback/feedback.component';
 import { PartnersComponent } from './common/partners/partners.component';
-import { DriverSignupComponent } from './driver/driver-signup/driver-signup.component';
 import { ServicesComponent } from './common/services/services.component';
 import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
+import { DriverSignupComponent } from './driver/driver-signup/driver-signup.component';
 import { DriverNavbarComponent } from './driver/driver-navbar/driver-navbar.component';
+import { DriverOrderStatusComponent } from './driver/driver-order-status/driver-order-status.component';
+import { DriverProfileComponent } from './driver/driver-profile/driver-profile.component';
 import { AdminDashboardComponent } from './admin/dashboard/dashboard.component';
 import { CustomerManagementComponent } from './admin/customer-management/customer-management.component';
 import { DriverManagementComponent } from './admin/driver-management/driver-management.component';
@@ -27,12 +29,14 @@ const routes: Routes = [
   { path: 'partners', component: PartnersComponent },
   { path: 'driverSignup', component: DriverSignupComponent },
   
-  { path: 'c-dashboard', component: CustomerDashboardComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'customer' }},
-  { path: 'order-placement', component: OrderPlacementComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'customer' } },
-  { path: 'track-order', component: OrderTrackingComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'customer' } },
-  { path: 'feedback', component: FeedbackComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'customer' } },
+  { path: 'c-dashboard', component: CustomerDashboardComponent, canActivate: [AuthGuard]},
+  { path: 'order-placement', component: OrderPlacementComponent, canActivate: [AuthGuard] },
+  { path: 'track-order', component: OrderTrackingComponent, canActivate: [AuthGuard] },
+  { path: 'feedback', component: FeedbackComponent, canActivate: [AuthGuard] },
 
-  { path: 'd-dashboard', component: DriverNavbarComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'driver' } },
+  { path: 'd-dashboard', component: DriverNavbarComponent, canActivate: [AuthGuard] },
+  { path: 'd-profile', component: DriverProfileComponent, canActivate: [AuthGuard] },
+  { path: 'd-order-status', component: DriverOrderStatusComponent, canActivate: [AuthGuard] },
 
   { path: 'a-dashboard', component: AdminDashboardComponent },
   { path: 'customer-details', component: CustomerManagementComponent },
